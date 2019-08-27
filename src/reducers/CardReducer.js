f9e81c6210d228fa7e6f8e19a40ddacdbf4c08f9
Beni <bence312@gmail.com>
2019-08-27 12:17:37 +0200

    load image urls, reducer: select deck size, load board

--- src/reducers/CardReducer.js
+++ src/reducers/CardReducer.js
@@ -1,12 +1,49 @@
+import * as actionTypes from '../actions/actionTypes';
+import uuid from 'uuid/v1';
+
+let images = [];
+const req = require.context('../../public/images/cards', false, /.*\.png$/);
+req.keys().forEach(function (key) {
+  images.push({
+    src: key
+  })
+});
+
 const initialState = {
   cards: [],
-  firstGuess: {},
-  deckSize: 0,
-  tries: 0
+  images: images,
+  firstGuess: null,
+  deckSize: null,
+  tries: null
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionTypes.SELECT_DECK_SIZE: {
+      return {
+        ...state,
+        deckSize: action.payload.deckSize
+      }
+    }
+    case actionTypes.LOAD_BOARD: {
+      const cards = [];
+      const pairs = state.deckSize / 2;
+      for(let i = 0; i < pairs; i++) {
+        cards.push({
+          id: uuid(),
+          image: state.images[i]
+        });
+        cards.push({
+          id: uuid(),
+          image: state.images[i]
+        });
+      }
+      return {
+        ...state,
+        cards: cards,
+        tries: 0
+      }
+    }
     default: {
       return state
     }
